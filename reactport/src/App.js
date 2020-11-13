a0214be80035ee239f10169256ded91fac1a62c2
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-11-13 00:52:53 -0500

    Added ClubStaff and Weather Dashboard. List of Skills

--- reactport/src/App.js
+++ reactport/src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div>
       <Navbar />
       <h1>Pierre Calmels</h1>
+      <h2>Web Developer</h2>
+      <ul>
+        <li>Javascript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>MySQL</li>
+      </ul>
       <Projects />
     </div>
   );
--- reactport/src/components/Projects.js
+++ reactport/src/components/Projects.js
@@ -1,44 +1,42 @@
 import React from "react";
 
 // import "../styles/Team.css";
-// import Mathew from "./Images/mathew (1).png";
-// import Aimee from "./Images/aimee (1).png";
-// import Pierre from "./Images/pierre (1).png";
+
 import Pierre from "../img/profile.original.jpg";
 import Container from "react-bootstrap/Container";
 import NoteTaker from "../img/NoteTaker.png";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Template from "../img/TemplateEngineHtml.png";
+import Merritt from "../img/MerrittHuntClub.png";
+import Weather from "../img/WeatherDashboard.png";
 // import Container from "react-bootstrap/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function Projects() {
   return (
     <Container>
-      <CardDeck>
-        <Card>
-          <Card.Img src={Pierre} alt="pierre" height="330px" />
-          <Card.Body>
-            <Card.Title>Pierre Calmels</Card.Title>
-            <Card.Text>Developer Portfolio</Card.Text>
-            <Card.Text></Card.Text>
-            <Card.Text>
-              <a href="https://github.com/pierre8237">
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      <Card>
+        <Card.Img src={Pierre} alt="pierre" height="330px" />
+        <Card.Body>
+          <Card.Text>Developer Portfolio</Card.Text>
+          <Card.Title>Pierre Calmels</Card.Title>
+          <Card.Text></Card.Text>
+          <Card.Text>
+            <a href="https://github.com/pierre8237">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <CardDeck style={{ width: "100%" }}>
         <Card>
           <Card.Img src={Template} alt="" height="330px" />
           <Card.Body>
-            <Card.Title></Card.Title>
+            <Card.Title>Employee Template Engine</Card.Title>
             <a href="https://github.com/pierre8237/Employee_Summary">
               <FontAwesomeIcon icon={faGithub} />
             </a>
-            <Card.Text></Card.Text>
-            <Card.Text></Card.Text>
             <Card.Text></Card.Text>
           </Card.Body>
         </Card>
@@ -47,9 +45,34 @@ function Projects() {
           <Card.Body>
             <Card.Title>Note Takin</Card.Title>
             <Card.Text></Card.Text>
-            <Card.Text>pierre8237@yahoo.com</Card.Text>
             <Card.Text>
               <a href="https://github.com/pierre8237/note-takin">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardDeck>
+      <CardDeck>
+        <Card>
+          <Card.Img src={Merritt} alt="Pierre" height="330px" />
+          <Card.Body>
+            <Card.Title>Merritt Hunt Club</Card.Title>
+            <Card.Text></Card.Text>
+            <Card.Text>
+              <a href="https://github.com/pierre8237/clubStaff">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img src={Weather} alt="Pierre" height="330px" />
+          <Card.Body>
+            <Card.Title>Open Weather Dashboard</Card.Title>
+            <Card.Text></Card.Text>
+            <Card.Text>
+              <a href="https://github.com/pierre8237/Weather-Tracker">
                 <FontAwesomeIcon icon={faGithub} />
               </a>
             </Card.Text>
